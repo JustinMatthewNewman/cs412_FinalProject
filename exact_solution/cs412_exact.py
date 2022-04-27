b7601817eff9d3d72e573374d0d51e33694a1d44
JustinMatthewNewman <newma4jm@dukes.jmu.edu>
2022-04-27 13:58:29 -0400

    Updated Exact, Added test generator

--- exact_solution/cs412_exact.py
+++ exact_solution/cs412_exact.py
@@ -1,49 +1,48 @@
-"""
-    Exact Solution to Max Knapsack using Brute Force.
-"""
-def max_profit(weight, stack):
-    for item in stack: # O(n)
-        item.append(item[1]/item[2])
-    stack.sort(key=lambda x: x[3]) #O(nlog(n))
-    bag = []
-    while sum(item[2] for item in bag) < weight or len(stack) != 0: # O(n)
-        if weight - item[2] > 0:
-            item = stack.pop()
-            weight -= item[2]
-            A[0] += item[1]
-            if (item[2] > 0): bag.append(item)
-        else: break
-    return bag
+import numpy as np
+import time
+def knapsack_dp(values,weights,n_items,capacity):
+    table = np.zeros((n_items+1,capacity+1))
+    keep = np.zeros((n_items+1,capacity+1))
+    for i in range(1,n_items+1):
+        for w in range(0,capacity+1):
+            wi = weights[i-1]
+            vi = values[i-1]
+            if (wi <= w) and (vi + table[i-1,w-wi] > table[i-1,w]):
+                table[i,w] = vi + table[i-1,w-wi]
+                keep[i,w] = 1
+            else:
+                table[i,w] = table[i-1,w]
+    picks = []
+    K = capacity
+    for i in range(n_items,0,-1):
+        if keep[i,K] == 1:
+            picks.append(i)
+            K -= weights[i-1]
+    picks = [x-1 for x in picks] # change to 0-index
+    return picks
 
-def knapSack(W, items, n):
-    bag = []
-    wt = [item[2] for item in items]
-    val = [item[1] for item in items]
-    dp = [0 for i in range(W+1)]
-    for i in range(1, n+1):
-        for w in range(W, 0, -1):
-            if wt[i-1] <= w:
-                dp[w] = max(dp[w], dp[w-wt[i-1]]+val[i-1])
-    A[0] = dp[W]
-    return bag
-
-A = [0]
-def main(): 
-    weight = int(input())
-    n = int(input())
-    lines = [input().split(" ") for _ in range(n)] # O(n)
-    for line in lines: # O(n)
-        line[1] = float(line[1])
-        line[2] = int(line[2])
-    # bag = max_profit(weight, lines) # O(nlog(n))
-    bag  = knapSack(weight, lines, n) # O(n*W)
-    print(A[0])
-    # for a in bag: # O(n)
-    #     if a == bag[-1]: print(a[0] + "(" + "{a:.2f}, {b:.2f}"
-    #     .format(a = a[1], b = a[2]) + ")", end="")
-    #     else: print(a[0] + "(" + "{a:.2f}, {b:.2f}"
-    #     .format(a = a[1], b = a[2]) + ")", end=" ")
-    # print("\n" + str(A[0]))
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    start = time.time()
+    capacity = int(input())
+    n_items = int(input())
+    lines = [input().split() for _ in range(n_items)]
+    names=[]
+    values=[]
+    weights=[]
+    for line in lines:
+        names.append(line[0])
+        values.append(int(line[1]))
+        weights.append(int(line[2]))
+    picks = knapsack_dp(values,weights,n_items,capacity)
+    max_val = 0
+    total_weight = 0
+    print("\nSelected items: \n")
+    for a in picks:
+        print(names[a], values[a], weights[a])
+        max_val += values[a]
+        total_weight += weights[a]
+    print("\nTotal value: ", max_val, "\nTotal weight: ", total_weight)
+    print()
+    end = time.time()
+    print(end - start, "seconds\n")
+    
