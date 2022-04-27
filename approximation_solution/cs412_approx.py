48b8eb667aa6525f201340cf51d6504a7b745220
Kyle LaCanna <lacannkj@dukes.jmu.edu>
2022-04-27 17:29:36 -0400

    output finished

--- approximation_solution/cs412_approx.py
+++ approximation_solution/cs412_approx.py
@@ -10,17 +10,19 @@ def main():
     items = [[y[0], float(y[1]), float(y[2])] for y in [input().split() for i in range(numItems)]]
     func = lambda list: list[1]/list[2] #price/weight
     func2 = lambda list: list[2]
+    func3 = lambda list: list[1]
     sorted_items = sorted(items, key=func) #list of items sorted from lowest value per weight to highest
     sorted_weights = sorted(items, key=func2)[::-1]
-    print(sorted_weights)
 
     # Use Fractional Knapsack to find an upperbound for correctness checking later on
     upperBound = int(FindUpperBound(sorted_items, maxW))
-    print(upperBound)
+    print("Upper Bound", upperBound)
     ret = FindApproxSolution(sorted_weights, upperBound, maxW)
-    print(ret)
-    print(getValue(ret))
-    #sorted_items = sorted_items[::-1]
+
+    sorted_ret = sorted(ret, key=func3)[::-1]
+    for item in sorted_ret:
+        print(item[0] + "(" + str(item[1]) + ", " + str(item[2]) + ")", end=" ")
+    print("\nApproximated Value", getValue(ret))
 
 def FindUpperBound(items, wRem):
     value = 0
@@ -38,14 +40,13 @@ def FindApproxSolution(items, upperBound, maxW):
     #untill our best solution is within a error range of our upper bound we continue to run a while loop
     #the while loop will keep picking items at random until it finds a full or close to full bag
 
-    #when within the error margin return the list of items and the value back to main
-
+    #when within the error margin return the list of items back to main
     resetable_items = items.copy()
     chosen_items = []
     max = 0
     error = (upperBound - max) / upperBound
 
-    while error > 0.15:
+    while error > 0.1:
         wRem = maxW
         resetable_items = items.copy()
         chosen_items = []
@@ -64,7 +65,7 @@ def getValue(items):
     value = 0
     for item in items:
         value += item[1]
-    return value
+    return int(value)
 
 def chooseRandomItemFits(items, wRem):
     #randomly picks and item out of the list and returns it if item's weight <= wRem
