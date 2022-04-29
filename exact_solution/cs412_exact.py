f055111c9f42109827dc0403c8ecace4fcc1a5a8
JustinMatthewNewman <newma4jm@dukes.jmu.edu>
2022-04-29 15:29:33 -0400

    added shell outputs

--- exact_solution/cs412_exact.py
+++ exact_solution/cs412_exact.py
@@ -20,6 +20,33 @@ def knapsack_dp(values,weights,n_items,capacity):
     picks = [x-1 for x in picks] # change to 0-index
     return picks
 
+# A naive recursive implementation O(2^n)
+def knapSack(W, wt, val, n):
+    # Base Case
+    if n == 0 or W == 0:
+        return 0
+    # If weight of the nth item is
+    # more than Knapsack of capacity W,
+    # then this item cannot be included
+    # in the optimal solution
+    if (wt[n-1] > W):
+        return knapSack(W, wt, val, n-1)
+    # return the maximum of two cases:
+    # (1) nth item included
+    # (2) not included
+    else:
+        return max(
+            val[n-1] + knapSack(
+                W-wt[n-1], wt, val, n-1),
+            knapSack(W, wt, val, n-1))
+
+#Driver Code
+# val = [60, 100, 120]
+# wt = [10, 20, 30]
+# W = 50
+# n = len(val)
+# print knapSack(W, wt, val, n)
+
 if __name__ == '__main__':
     start = time.time()
     capacity = int(input())
@@ -33,12 +60,12 @@ if __name__ == '__main__':
         values.append(int(line[1]))
         weights.append(int(line[2]))
     picks = knapsack_dp(values,weights,n_items,capacity)
-    max_val = 0
-    total_weight = 0
+    max_val = knapSack(capacity, weights, values, n_items)
+    #total_weight = 0
     # print("\nSelected items: \n")
-    for a in picks:
+    #for a in picks:
         #print(names[a], values[a], weights[a])
-        max_val += values[a]
+        #max_val += values[a]
         #total_weight += weights[a]
     # print("\nTotal value: ", max_val, "\nTotal weight: ", total_weight)
     print(max_val, end=" ")
